ac05bed7f404c0256d24ab91e6cc18fb0ce1a87f
riccardo__ruggieri <riccardo.ruggieri@mail.polimi.it>
2024-11-18 09:09:41 +0100

    added sampling percentage for speech_commands.py

--- Dataset/classification/utils/speech_commands.py
+++ Dataset/classification/utils/speech_commands.py
@@ -27,36 +27,43 @@ def download():
         print("Extraction completed.")
 
 
-def _process_data(intensity_data):
+import random
+
+def _process_data(intensity_data, percentage=10):
     base_loc = here / 'data' / 'SpeechCommands'
     X = torch.empty(34975, 16000, 1)
     y = torch.empty(34975, dtype=torch.long)
 
+    total_samples = int(34975 * (percentage / 100))  # Calculate number of samples
     batch_index = 0
     y_index = 0
+
     for foldername in ('yes', 'no', 'up', 'down', 'left', 'right', 'on', 'off', 'stop', 'go'):
         loc = base_loc / foldername
-        for filename in os.listdir(loc):
-            # audio, _ = torchaudio.load_wav(loc / filename, channels_first=False,
-            #                                normalization=False)  # for forward compatbility if they fix it
-            audio, _ = torchaudio.load(loc / filename, channels_first=False,
-                                       normalize=True)  # for forward compatbility if they fix it
-            audio = audio / 2 ** 10  # Normalization argument doesn't seem to work so we do it manually.
+        files = os.listdir(loc)
 
-            # A few samples are shorter than the full length; for simplicity we discard them.
+        # Randomly select a subset of files proportional to the percentage
+        selected_files = random.sample(files, int(len(files) * (percentage / 100)))
+
+        for filename in selected_files:
+            audio, _ = torchaudio.load(loc / filename, channels_first=False, normalize=True)
+
+            # Discard shorter samples for simplicity
             if len(audio) != 16000:
                 continue
 
+            audio = audio / 2 ** 10  # Normalize manually
             X[batch_index] = audio
             y[batch_index] = y_index
             batch_index += 1
         y_index += 1
-    assert batch_index == 34975, "batch_index is {}".format(batch_index)
+
+    X = X[:batch_index]  # Trim unused preallocated space
+    y = y[:batch_index]
 
     X = torchaudio.transforms.MFCC(log_mels=True, n_mfcc=20,
                                    melkwargs=dict(n_fft=200, hop_length=100, n_mels=128))(X.squeeze(-1)).transpose(1, 2).detach()
     print(X.shape)
-    # X is of shape (batch=34975, length=161, channels=20)
 
     times = torch.linspace(0, X.size(1) - 1, X.size(1))
     final_index = torch.tensor(X.size(1) - 1).repeat(X.size(0))
@@ -67,6 +74,7 @@ def _process_data(intensity_data):
 
     return (times, train_coeffs, val_coeffs, test_coeffs, train_y, val_y, test_y, train_final_index, val_final_index,
             test_final_index)
+
 
 
 def get_data(intensity_data, batch_size):
@@ -87,7 +95,7 @@ def get_data(intensity_data, batch_size):
     else:
         download()
         (times, train_coeffs, val_coeffs, test_coeffs, train_y, val_y, test_y, train_final_index, val_final_index,
-         test_final_index) = _process_data(intensity_data)
+         test_final_index) = _process_data(intensity_data, percentage=10)
         if not os.path.exists(base_base_loc):
             os.mkdir(base_base_loc)
         if not os.path.exists(loc):
